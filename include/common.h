3f6608133f9110279612a53eaa36471185e08f0a
0x30c4 <abdullahsanaf@gmail.com>
2022-02-19 21:54:31 +0600

    Starting

--- include/common.h
+++ include/common.h
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <termios.h>
-#include <math.h>
-#include <time.h>
 
 /*----------------------------------------------------------------
     Here I'm defining TRUE and FALSE and typedefing short as bool.
@@ -16,18 +13,6 @@
 #define TRUE   1
 #define FALSE  0
 
-/*----------------------------------------------------------------
-    If a user presses this `EXIT_CHAR` then the game will stop.
-*/
-
-#define EXIT_CHAR 'q'
-
 typedef short bool;
-
-typedef struct mapInfo_ {
-   int metadata_amount;
-   int map_row;
-   int map_col;
-} mapInfo_t;
 
 #endif
